a39060986112d994a4064060ed50a6af1d9cbbc4
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2021-11-28 22:06:53 -0300

    init project

---
+++ Tinder/Tinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Paulo Rodrigues on 28/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
